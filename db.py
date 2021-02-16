51cb63e50079f1a149a1e4fcadd270b83b35cf4f
Rui Azevedo <49915263+Ruizao@users.noreply.github.com>
2021-02-16 14:55:17 +0000

    Initial commit

---
+++ db.py
@@ -0,0 +1,7 @@
+class Database:
+    def __init__(self):
+        self.Host = 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com'
+        self.Database = 'd4hnnuaetmsoit'
+        self.User = 'bqsfvscdserdjh'
+        self.Password = '5d000613c81bbc48e1a49379b19f50e989a198d4aba88a5a956bbdc8ee7edfdb'
+
